a80127f9c40eeb943ab435f745d1aec8be1e6453
Douglas Araújo <araujodgdev@gmail.com>
2025-05-05 12:19:04 -0300

    fix: eslint errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
