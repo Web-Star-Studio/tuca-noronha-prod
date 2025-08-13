4d9194f46f88f21baa7e26008388eaf1d1e4272f
Douglas Araújo <araujodgdev@gmail.com>
2025-08-13 18:52:57 -0300

    update clerk reference o convex

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: 'https://harmless-polecat-2.clerk.accounts.dev',
+      domain: process.env.CLERK_DOMAIN,
       applicationID: "convex",
     },
   ],
