cbbfe0f6fba2b00bfc59d2a7653591b1f3d6d377
Douglas Araújo <araujodgdev@gmail.com>
2025-09-17 14:08:54 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TripNative",
+  title: "Tuca Noronha",
   description: "Experiências turísticas únicas e autênticas",
 };
 
